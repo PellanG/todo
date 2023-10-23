66219e468e2884fb7e2a0c4354d374829ca74f53
Ellinore Gustafsson <pellan@airsomtellinore.lan>
2023-10-23 13:20:20 -0700

    working with task-container js

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 import "./SCSS/main.scss";
+class Listitem{
+    name;
+    done;
+
+    constructor(name,done){
+        this.name=name;
+        this.done=done;
+    }
+}
+
+const item1=new Listitem("Städa",false);
+const item2=new Listitem("Diska",true);
+const item3=new Listitem("Handla",true);
+
+const toDoList=[item1,item2,item3];
+
+let listContainer=document.getElementById("list");
+
+
+function printList(){
+    
+    for (let i = 0; i < toDoList.length; i++) { 
+        let listItem=document.getElementById("listitem");
+        const finishedTask = document.createElement("input");
+         if (true) {
+            listItem.innerHTML=(toDoList[i]).name;
+            finishedTask.type="checkbox";
+            finishedTask.className="checkbox";
+            finishedTask.checked=(toDoList[i]).done;
+
+              
+         }
+            listItem.innerHTML=(toDoList[i]).name;
+            console.log(toDoList[i]);
+            listItem.appendChild(finishedTask);
+            listContainer.appendChild(listItem);
+    }
+
+    }
+   
+   
+
+
+printList();
