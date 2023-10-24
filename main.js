f7c071263bb269cd1fd62212125405a032e6c549
Ellinore Gustafsson <pellan@MacBook-Air-som-tillhor-Ellinore.local>
2023-10-24 15:19:08 -0700

    finished add task function

--- main.js
+++ main.js
@@ -11,22 +11,27 @@ class Listitem{
     }
 }
 function creatHtml(){//Skapar html för listan.
-    
+    const listOfTask=document.getElementById("list");
+    listOfTask.innerHTML="";
     for (let i = 0; i < toDoList.length; i++) { 
         
         const listItem=document.createElement("li");
+        const taskName=document.createElement("span");
         const finishedTask = document.createElement("input");
         const isDeleted=document.createElement("span");
         const closeBtn=document.createTextNode("\u00D7");
         
+
         isDeleted.className="closeItem";
         finishedTask.type="checkbox";
         finishedTask.className="checked";
-    
+        
         finishedTask.checked=toDoList[i].done;
-        listItem.innerHTML=toDoList[i].name;
+        taskName.innerHTML=toDoList[i].name;
+        
         
         listItem.appendChild(finishedTask);
+        listItem.appendChild(taskName);
         isDeleted.appendChild(closeBtn);
         listItem.appendChild(isDeleted);
         listContainer.appendChild(listItem);
@@ -34,7 +39,7 @@ function creatHtml(){//Skapar html för listan.
         
     
     }  
-    function creatHtmlNewItem(){//Skapar html för nya listobjekt(ej pushat till lista)
+function creatHtmlNewItem(){//Skapar html för nya listobjekt(ej pushat till lista)
         const newUserInput=document.getElementById("userinput").value;
         const newlistItem=document.createElement("li");
         const inputValue = document.createTextNode(newUserInput);
@@ -46,13 +51,26 @@ function creatHtml(){//Skapar html för listan.
         newfinishedTask.type="checkbox";
         newfinishedTask.className="checked";
         
-        newlistItem.appendChild(inputValue);
         newlistItem.appendChild(newfinishedTask);
+        newlistItem.appendChild(inputValue);
         newisDeleted.appendChild(newcloseBtn);
         newlistItem.appendChild(newisDeleted);
         listContainer.appendChild(newlistItem);
     
     }
+
+function closeTask(){
+    const close=document.getElementsByClassName("closeItem");
+    console.log(close);
+    
+}
+
+const item1=new Listitem("Städa",false);
+const item2=new Listitem("Diska",false);
+const item3=new Listitem("Handla",false);
+
+const toDoList=[item1,item2,item3];
+
 const addTask = (e)=>{ //Lägger till det användaren skriver i listan
     e.preventDefault();
     const addTaskUserinput=document.getElementById("userinput").value;
@@ -62,20 +80,21 @@ const addTask = (e)=>{ //Lägger till det användaren skriver i listan
         
     }
     else{
-    console.log("det funkar");
-    creatHtmlNewItem(addTaskUserinput);
+    const newItem = new Listitem(addTaskUserinput,false);
+    console.log(newItem);
+    toDoList.push(newItem);
+    creatHtml();
+    
+    
     
     document.getElementById("userinput").value="";
     }
     
 }
-const item1=new Listitem("Städa",false);
-const item2=new Listitem("Diska",false);
-const item3=new Listitem("Handla",false);
-
-const toDoList=[item1,item2,item3];
 
 let listContainer=document.getElementById("list");
 const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click",addTask);
 creatHtml();
+
+
