7c7cd5405fbcc29bfd5b5752f6d3ab01b2ae2d8d
Ellinore Gustafsson <pellan@airsomtellinore.lan>
2023-10-23 19:27:35 -0700

    working on input-container js

--- main.js
+++ main.js
@@ -10,56 +10,52 @@ class Listitem{
 }
 
 const item1=new Listitem("Städa",false);
-const item2=new Listitem("Diska",true);
-const item3=new Listitem("Handla",true);
+const item2=new Listitem("Diska",false);
+const item3=new Listitem("Handla",false);
 
 const toDoList=[item1,item2,item3];
 
 let listContainer=document.getElementById("list");
 
 
-function printList(taskFromUser){
+function printList(newTask){
     
     for (let i = 0; i < toDoList.length; i++) { 
         
         let listItem=document.getElementById("listitem");
         const finishedTask = document.createElement("input");
-        toDoList.push=taskFromUser;
+        console.log(newTask);
         
-         if (true) {
-            listItem.innerHTML=(toDoList[i]).name;
-            finishedTask.type="checkbox";
-            finishedTask.className="checkbox";
-            finishedTask.checked=(toDoList[i]).done;
 
-              
-         }
-            listItem.innerHTML=(toDoList[i]).name;
-            console.log(toDoList[i]);
-            listItem.appendChild(finishedTask);
-            listContainer.appendChild(listItem);
-            
+        finishedTask.type="checkbox";
+        finishedTask.className="checked";
+        finishedTask.checked=toDoList[i].done;
+        listItem.innerHTML=toDoList[i].name;
+        listItem.appendChild(finishedTask);
+        listContainer.appendChild(listItem);
         }
     
     }
-
+    
 const addTask = (e)=>{
     e.preventDefault();
-
     const addTaskUserinput=document.getElementById("userinput").value;
 
     if (addTaskUserinput==="") {
         alert("You forgot to write something to do.");
         
     }
-    console.log("du klickade");
-    const taskFromUser=document.getElementById("userinput").value;
+    else{
+    console.log("det funkar");
+    const newTask=document.createElement("li");
+    document.getElementById("list").appendChild(newTask);
+    
     document.getElementById("userinput").value="";
+    console.log(newTask);
+    return newTask();
+    }
+    
 }
-
-
-document.getElementById("input-container-form").addEventListener("submit",addTask);
-
 const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click",addTask);
 printList();
